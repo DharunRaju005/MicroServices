9d6899add80c69ce0cabc571523e9ef8dbe1d1bb
DharunRaju005 <dharunraju22@gmail.com>
2025-05-24 19:27:27 +0530

    Integrated kafka in dsservice

--- dsservice/src/app/__init__.py
+++ dsservice/src/app/__init__.py
@@ -1,19 +1,26 @@
 from flask import Flask
 from flask import request,jsonify
 from service.messageService import MessageService
+from kafka import KafkaProducer
+import json
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
+
 messageService = MessageService()
+producer=KafkaProducer(bootstrap_server=['localhost:9092'],value_serializer=lambda v:json.dumps(v).encode('utf-8'))
+
 @app.route('/v1/ds/message/',methods=['POST'])
 def handle_message():
     message=request.json.get('message')
     result=messageService.process_message(message)
-    return jsonify(result)
+    serialised_result=result.json();
+    producer.send("expense_service",serialised_result);
+    return jsonify(result);
     
 @app.route('/',methods=['GET'])
 def handle_get():
-    return "U Made it Bhai"
+    return "jeichuta maara"
 
 if __name__=="__main__":
     app.run(host="localhost",port=8000,debug=True)
